7891f34174a677f245734c81014ade2e55eb6b12
一剑 <answord@163.com>
2019-11-19 19:36:34 +0800

    c++端定义dll的相关接口

--- starrtcdemo/starrtcdemo/VOIPForCSharpe.cpp
+++ starrtcdemo/starrtcdemo/VOIPForCSharpe.cpp
@@ -41,7 +41,7 @@ BOOL YPLogin(char* localUserId)
 
 BOOL YPCall(char* targetUserId)
 {
-	AFX_MANAGE_STATE(AfxGetStaticModuleState());
+	//AFX_MANAGE_STATE(AfxGetStaticModuleState());
 
 	if (!checkVoip())
 	{
@@ -62,22 +62,47 @@ BOOL YPCall(char* targetUserId)
 
 void YPCancel()
 {
+	if (!checkVoip())
+	{
+		// 未登陆，不允许调用相关方法
+		return;
+	}
 
+	ypVoip->cancel();
 }
 
 void YPAccept(char* fromID)
 {
+	if (!checkVoip())
+	{
+		// 未登陆，不允许调用相关方法
+		return;
+	}
 
+	string strFromID = fromID;
+	ypVoip->accept(strFromID);
 }
 
 void YPRefuse()
 {
+	if (!checkVoip())
+	{
+		// 未登陆，不允许调用相关方法
+		return;
+	}
 
+	ypVoip->refuse();
 }
 
 void YPHangup(int isActive)
 {
+	if (!checkVoip())
+	{
+		// 未登陆，不允许调用相关方法
+		return;
+	}
 
+	ypVoip->hangup(isActive);
 }
 
 void YPDeleteVoip()
@@ -91,4 +116,43 @@ void YPDeleteVoip()
 void YPOnCalling(OnCallingCallback callback)
 {
 	ypVoip->pOnCalling = callback;
+}
+
+void YPOnCancled(OnCancledCallback callback)
+{
+	ypVoip->pOnCancled = callback;
+}
+void YPOnRefused(OnCancledCallback callback)
+{
+	ypVoip->pOnRefused = callback;
+}
+
+void YPOnBusy(OnBusyCallback callback)
+{
+	ypVoip->pOnBusy = callback;
+}
+
+void YPOnConnected(OnConnectedCallback callback)
+{
+	ypVoip->pOnConnected = callback;
+}
+
+void YPOnHangup(OnHangupCallback callback)
+{
+	ypVoip->pOnHangup = callback;
+}
+
+void YPOnError(OnErrorCallback callback)
+{
+	ypVoip->pOnError = callback;
+}
+
+void YPOnGetSelfVideoRaw(OnGetVideoRawCallback callback)
+{
+	ypVoip->pOnGetSelfVideoRaw = callback;
+}
+
+void YPOnGetOtherVideoRaw(OnGetVideoRawCallback callback)
+{
+	ypVoip->pOnGetOtherVideoRaw = callback;
 }
--- starrtcdemo/starrtcdemo/VOIPForCSharpe.h
+++ starrtcdemo/starrtcdemo/VOIPForCSharpe.h
@@ -4,23 +4,48 @@
 using namespace std;
 
 typedef void(__stdcall *OnCallingCallback)(char* fromID);
+typedef void(__stdcall *OnCancledCallback)(char* fromID);
+typedef void(__stdcall *OnRefusedCallback)(char* fromID);
+typedef void(__stdcall *OnBusyCallback)(char* fromID);
+typedef void(__stdcall *OnConnectedCallback)(char* fromID);
+typedef void(__stdcall *OnHangupCallback)(char* fromID);
+typedef void(__stdcall *OnErrorCallback)(char* errorCode);
+
+typedef void(__stdcall *OnGetVideoRawCallback)(char* userId, int w, int h, HBITMAP videoData, int videoDataLen);
 
 extern "C" {
 	// 登陆(一定要先调用登陆并登陆成功，才能调用其他函数)
     __declspec(dllexport) BOOL YPLogin(char* localUserId);
-	// 发起呼叫
+	// 发起通话（主叫方调用）
 	__declspec(dllexport) BOOL YPCall(char* targetUserId);
-	// 取消呼叫
+	// 主叫方主动取消呼叫（主叫方调用）
 	__declspec(dllexport) void YPCancel();
-	// 同意通话
+	// 同意跟主叫方通话（被叫方调用）
 	__declspec(dllexport) void YPAccept(char* fromID);
-	// 拒绝通话
+	// 拒绝跟主叫方通话（被叫方调用）
 	__declspec(dllexport) void YPRefuse();
-	// 挂断
+	// 挂断（双方都可调用）
 	__declspec(dllexport) void YPHangup(int isActive);
 	// 释放
 	__declspec(dllexport) void YPDeleteVoip();
 
-	// 收到呼叫
+	// 被叫方收到主叫方的呼叫（被叫方响应）
 	__declspec(dllexport) void YPOnCalling(OnCallingCallback callback);
+	// 主叫方在被叫方接听之前挂断，即通话被取消（被叫方响应）
+	__declspec(dllexport) void YPOnCancled(OnCancledCallback callback);
+	// 被叫方拒绝接通（主叫方响应）
+	__declspec(dllexport) void YPOnRefused(OnCancledCallback callback);
+	// 被叫方线路忙（主叫方响应）
+	__declspec(dllexport) void YPOnBusy(OnBusyCallback callback);
+	// 被叫方接通，通话开始（主叫方响应）
+	__declspec(dllexport) void YPOnConnected(OnConnectedCallback callback);
+	// 对方已经挂断（双方都可响应）
+	__declspec(dllexport) void YPOnHangup(OnHangupCallback callback);
+	// 收到voip报错
+	__declspec(dllexport) void YPOnError(OnErrorCallback callback);
+
+	// 收到已方视频数据
+	__declspec(dllexport) void YPOnGetSelfVideoRaw(OnGetVideoRawCallback callback);
+	// 收到对方视频数据
+	__declspec(dllexport) void YPOnGetOtherVideoRaw(OnGetVideoRawCallback callback);
 }
